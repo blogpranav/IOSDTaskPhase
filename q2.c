bac4985b6bf47a142562872c87d9fd0e7e8702c4
harsh vora <harsh.k.vora@gmail.com>
2020-02-09 00:17:07 +0530

    cp_harsh

--- q2.c
+++ q2.c
@@ -1,1 +1,44 @@
+#include<stdio.h>
+int min_plates_of_momos(int m, int arr[], int n)
+{
+    int sum=0;
+    int count=0;
+    for(int i=n-1; i>=0; i--)
+    {
+        sum=sum+arr[i];
+        count++;
+        if(sum>=m)
+            return count;
 
+    }
+    return -1;
+}
+void main()
+{
+	int t;
+	int temp;
+	scanf("%d",&t);
+    while(t--)
+    {
+        int n,m;
+        int i;
+        int j;
+        scanf("%d %d",&n,&m);
+        int arr[n];
+        for( i=0; i<n; i++)
+            scanf("%d",&arr[i]);
+        for(i=0;i<n;i++)
+        {
+        	for(j=0;j<n-i-1;j++)
+        	{
+        		if(arr[j]>arr[j+1])
+        		{
+        			temp=arr[j];
+        			arr[j]=arr[j+1];
+        			arr[j+1]=temp;
+        		}
+        	}
+        }
+        printf("%d\n",min_plates_of_momos(m,arr,n));
+    }
+}
